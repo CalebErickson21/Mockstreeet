1396877a0d40fd5e478831090725aa164c3ebc8c
Caleb Erickson <coding.c9@gmail.com>
2025-02-10 23:57:50 +0000

    Reconfigured backend to use environment variables file to prevent hardcoding sensitive information, database is set up on backend, just need to set up middleware for communication between frontend and backend

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,14 @@
+// psql -U postgres -d mockstreet -h localhost -p 5432
+// Dependencies
 const { Client } = require('pg');
+require('dotenv').config();
 
 const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'mockstreet',
-    password: '#BamaDB21!',
-    port: 5432,
+  connectionString: process.env.DATABASE_URL
 });
 
 client.connect()
   .then(() => console.log('Connected to PostgreSQL database!'))
   .catch((err) => console.error('Connection error', err.stack));
+
+module.exports = client;
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,24 @@
-// Require DB
-require("./db");
-
+// Import dependencies
+const client = require('./db');
 const express = require('express');
+const session = require('express-session');
+require('dotenv').config();
+
+// Import express app and middleware
 const app = express();
+app.use(express.json());
+
+// Define port
 const PORT = 5000;
 
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, httpOnly: true, maxAge: 1000 * 60 * 60 }  // 1-hour session
+}));
+
+// Define route
 app.get('/', (req, res) => {
     res.send('Hello World from backend Node.js!');
 });
