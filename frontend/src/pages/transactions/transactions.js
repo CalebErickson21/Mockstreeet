f8625693408933339d2258761c2a36b0a40fab20
Caleb Erickson <coding.c9@gmail.com>
2025-02-15 06:07:51 +0000

    Added transactions layout, now just need to pull data from backend, add user validation, and test before deploying

--- frontend/src/pages/transactions/transactions.js
+++ frontend/src/pages/transactions/transactions.js
@@ -3,8 +3,56 @@ import './transactions.scss';
 
 const Transactions = () => {
     return (
-        <div id='transaction-container'>
+        <div id='transactions-container'>
 
+            <div id='filters'>
+                <h5>Filter By:</h5>
+                <div class='row'>
+                    <div class='col col-4'>
+                        <select class='form-select'>
+                            <option selected>Stock</option>
+                        </select>
+                    </div>
+
+                    <div class='col col-4'>
+                        <select class='form-select'>
+                            <option selected>Transaction Type</option>
+                        </select>
+                    </div>
+
+                    <div class='col col-4'>
+                        <select class='form-select'>
+                            <option selected>Date</option>
+                        </select>
+                    </div>
+                </div>
+
+            </div>
+
+            <div id='table-container'>
+                <table class='table table-striped scrollable'>
+                    <thead>
+                        <tr>
+                            <th>Stock</th>
+                            <th>Ticker</th>
+                            <th>Shares</th>
+                            <th>Total Price</th>
+                            <th>Transaction Type</th>
+                            <th>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Apple</td>
+                            <td>AAPL</td>
+                            <td>10</td>
+                            <td>$1,500</td>
+                            <td>Buy</td>
+                            <td>2025-02-14</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
 
 
 
